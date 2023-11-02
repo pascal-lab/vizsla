aae376868532ec968f5c80be047f1823ed2af83f
roife <roifewu@gmail.com>
2023-11-02 20:10:01 +0800

    add(main): add line_ending handling

--- src/line_idx.rs
+++ src/line_idx.rs
@@ -17,3 +17,47 @@ pub(crate) enum LineEndings {
     Unix,
     Dos,
 }
+
+impl LineEndings {
+    /// Replaces `\r\n` with `\n` in-place in `src`.
+    pub(crate) fn normalize(src: String) -> (String, LineEndings) {
+        // We replace `\r\n` with `\n` in-place, which doesn't break utf-8 encoding.
+        // While we *can* call `as_mut_vec` and do surgery on the live string
+        // directly, let's rather steal the contents of `src`. This makes the code
+        // safe even if a panic occurs.
+
+        let mut buf = src.into_bytes();
+        let mut gap_len = 0;
+        let mut tail = buf.as_mut_slice();
+        let mut crlf_seen = false;
+
+        let find_crlf = |src: &[u8]| src.windows(2).position(|it| it == b"\r\n");
+
+        loop {
+            let idx = match find_crlf(&tail[gap_len..]) {
+                None if crlf_seen => tail.len(),
+                // SAFETY: buf is unchanged and therefore still contains utf8 data
+                None => return (unsafe { String::from_utf8_unchecked(buf) }, LineEndings::Unix),
+                Some(idx) => {
+                    crlf_seen = true;
+                    idx + gap_len
+                }
+            };
+            tail.copy_within(gap_len..idx, 0);
+            tail = &mut tail[idx - gap_len..];
+            if tail.len() == gap_len {
+                break;
+            }
+            gap_len += 1;
+        }
+
+        // Account for removed `\r`.
+        // After `set_len`, `buf` is guaranteed to contain utf-8 again.
+        let src = unsafe {
+            let new_len = buf.len() - gap_len;
+            buf.set_len(new_len);
+            String::from_utf8_unchecked(buf)
+        };
+        (src, LineEndings::Dos)
+    }
+}
