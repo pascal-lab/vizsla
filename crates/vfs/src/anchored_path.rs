8ecb6fe87c61fe254dddc4a1e668019ff4e177a2
roife <roifewu@gmail.com>
2023-11-02 20:08:07 +0800

    refactor(vfs): rename fields

--- crates/vfs/src/anchored_path.rs
+++ crates/vfs/src/anchored_path.rs
@@ -2,12 +2,12 @@ use crate::FileId;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct AnchoredPathBuf {
-    pub anchor: FileId,
+    pub anchor_id: FileId,
     pub path: String,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct AnchoredPath<'a> {
-    pub anchor: FileId,
+    pub anchor_id: FileId,
     pub path: &'a str,
 }
