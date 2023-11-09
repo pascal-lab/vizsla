e9fa0fae1f2e8f69f19d479c529b056550e9fe30
roife <roifewu@gmail.com>
2023-11-09 15:01:18 +0800

    add(base-db): add durability for salsa

--- crates/base-db/src/change.rs
+++ crates/base-db/src/change.rs
@@ -1,3 +1,4 @@
+use salsa::Durability;
 use triomphe::Arc;
 
 use vfs::FileId;
@@ -21,5 +22,13 @@ impl Change {
 
     pub fn add_changed_file(&mut self, file_id: FileId, new_text: Option<Arc<str>>) {
         self.changed_files.push((file_id, new_text))
+    }
+}
+
+fn durability(source_root: &SourceRoot) -> Durability {
+    if source_root.is_library {
+        Durability::HIGH
+    } else {
+        Durability::LOW
     }
 }
