2181e8b4fb72cebdea92282ca9719367373d0803
roife <roifewu@gmail.com>
2023-12-29 14:51:54 +0800

    refactor(syntax): remove SyntaxError since ERROR is an syntax node in tree-sitter

--- crates/syntax/src/parse.rs
+++ crates/syntax/src/parse.rs
@@ -1,25 +1,6 @@
-use triomphe::Arc;
-use utils::text_edit::{TextRange, TextSize};
-
 use crate::SyntaxNode;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct SyntaxError(String, TextRange);
-
-impl SyntaxError {
-    pub fn new(message: impl Into<String>, range: TextRange) -> Self {
-        Self(message.into(), range)
-    }
-    pub fn new_at_offset(message: impl Into<String>, offset: TextSize) -> Self {
-        Self(message.into(), TextRange::empty(offset))
-    }
-
-    pub fn range(&self) -> TextRange {
-        self.1
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct SyntaxTree(tree_sitter::Tree);
 
 impl PartialEq for SyntaxTree {
@@ -30,39 +11,16 @@ impl PartialEq for SyntaxTree {
 
 impl Eq for SyntaxTree {}
 
-pub type SyntaxErrors = Arc<[SyntaxError]>;
-
-// `Parse` is the result of parsing
-#[derive(Debug, PartialEq, Eq)]
-pub struct Parse {
-    tree: SyntaxTree,
-    errors: Option<Arc<[SyntaxError]>>,
-}
-
-impl Parse {
-    fn new(tree: tree_sitter::Tree, errors: Vec<SyntaxError>) -> Parse {
-        Parse {
-            tree: SyntaxTree(tree),
-            errors: if errors.is_empty() { None } else { Some(errors.into()) },
-        }
+impl SyntaxTree {
+    pub fn new(tree: tree_sitter::Tree) -> Self {
+        Self(tree)
     }
 
     pub fn tree(&self) -> &tree_sitter::Tree {
-        &self.tree.0
+        &self.0
     }
 
     pub fn root_node(&self) -> SyntaxNode {
-        self.tree.0.root_node()
-    }
-
-    pub fn errors(&self) -> &[SyntaxError] {
-        self.errors.as_deref().unwrap_or_default()
-    }
-
-    pub fn ok(&self) -> Result<&SyntaxTree, SyntaxErrors> {
-        match self.errors.as_ref() {
-            Some(e) => Err(e.clone()),
-            None => Ok(&self.tree),
-        }
+        self.0.root_node()
     }
 }
