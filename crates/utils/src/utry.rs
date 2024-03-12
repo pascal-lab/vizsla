e6f5526d14894a445ff4733d4a35909e52c13f27
BryanHeBY <bryanheby@qq.com>
2024-03-12 21:50:22 +0800

    change(utils): add multiple exprs support for try!

--- crates/utils/src/utry.rs
+++ crates/utils/src/utry.rs
@@ -3,11 +3,20 @@ macro_rules! try_ {
     ($expr:expr) => {
         || -> _ { Some($expr) }()
     };
+
+    ($($tt:tt)*) => {
+        try_!{{$($tt)*}}
+    };
 }
 
 #[macro_export]
 macro_rules! try_or_default {
     ($expr:expr) => {
         try_!($expr).unwrap_or_default()
+    };
+
+
+    ($($tt:tt)*) => {
+        try_or_default!{{$($tt)*}}
     };
 }
