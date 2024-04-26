bc0d00f5cb11500c1bbbcda657ace0ab2c7392ee
roife <roifewu@gmail.com>
2024-04-26 20:06:07 +0800

    add(base-db): trait Intern and Lookup

---
+++ crates/base-db/src/intern.rs
@@ -0,0 +1,46 @@
+pub trait Intern {
+    type Database<'db>: ?Sized;
+    type ID;
+    fn intern(self, db: &Self::Database<'_>) -> Self::ID;
+}
+
+pub trait Lookup {
+    type Database<'db>: ?Sized;
+    type Data;
+    fn lookup(&self, db: &Self::Database<'_>) -> Self::Data;
+}
+
+#[macro_export]
+macro_rules! impl_intern_key {
+    ($name:ident) => {
+        impl $crate::salsa::InternKey for $name {
+            fn from_intern_id(v: $crate::salsa::InternId) -> Self {
+                $name(v)
+            }
+            fn as_intern_id(&self) -> $crate::salsa::InternId {
+                self.0
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_intern_lookup {
+    ($db:ident, $id:ident, $loc:ident, $intern:ident, $lookup:ident) => {
+        impl $crate::intern::Intern for $loc {
+            type Database<'db> = dyn $db + 'db;
+            type ID = $id;
+            fn intern(self, db: &Self::Database<'_>) -> $id {
+                db.$intern(self)
+            }
+        }
+
+        impl $crate::intern::Lookup for $id {
+            type Database<'db> = dyn $db + 'db;
+            type Data = $loc;
+            fn lookup(&self, db: &Self::Database<'_>) -> $loc {
+                db.$lookup(*self)
+            }
+        }
+    };
+}
--- crates/base-db/src/lib.rs
+++ crates/base-db/src/lib.rs
@@ -3,5 +3,6 @@
 pub use salsa::{self, Cancelled};
 
 pub mod change;
+pub mod intern;
 pub mod source_db;
 pub mod source_root;
