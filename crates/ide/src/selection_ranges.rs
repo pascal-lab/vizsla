be0127b168a86e7d387930431c0d23bfd70864a8
roife <roifewu@gmail.com>
2024-11-21 17:10:05 +0800

    fix(ide/selection_ranges): selections for trivias

--- crates/ide/src/selection_ranges.rs
+++ crates/ide/src/selection_ranges.rs
@@ -23,26 +23,43 @@ pub(crate) fn selection_ranges(
 
     let mut cursor = root.walk();
 
-    match root.token_at_offset(offset).pick_bext_token(token_precedence) {
+    let trivias_start = match root.token_at_offset(offset).pick_bext_token(token_precedence) {
         Some(token) => {
             cursor.goto_first_tok_after_or_last(token.text_range().unwrap().start());
+            None
         }
         None => {
             cursor.goto_first_tok_after_or_last(offset);
             let token = cursor.to_token().unwrap();
-            let start = token.trivias_with_range().unwrap().next().unwrap().0.start();
-            let end = token.text_range().unwrap().start();
-            res.push(TextRange::new(start, end));
+            let trivias = token.trivias_with_range().unwrap().collect_vec();
+            let range = trivias.iter().find(|(range, _)| range.contains(offset)).unwrap();
+            res.push(range.0);
+
+            let trivias_start = trivias[0].0.start();
+            res.push(TextRange::new(trivias_start, token.text_range().unwrap().start()));
+            Some(trivias_start)
+        }
+    };
+
+    let mut push_to_res = |range: TextRange| {
+        if let Some(trivias_start) = trivias_start {
+            if trivias_start < range.start() {
+                res.push(TextRange::new(trivias_start, range.end()));
+            } else {
+                res.push(range);
+            }
+        } else {
+            res.push(range);
         }
     };
 
     let mut range = cursor.to_token().unwrap().text_range().unwrap();
-    res.push(range);
+    push_to_res(range);
 
     while cursor.goto_parent() {
         let new_range = cursor.to_elem().text_range().unwrap();
         if new_range != range {
-            res.push(new_range);
+            push_to_res(range);
             range = new_range
         }
     }
