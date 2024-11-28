af0330f8bdf7f4e748ba6de32151a8580213622a
roife <roifewu@gmail.com>
2024-11-29 05:25:49 +0800

    fix(utils): correct line range calculation to include end line

--- crates/utils/src/line_index.rs
+++ crates/utils/src/line_index.rs
@@ -216,7 +216,7 @@ impl LineIndex {
     pub fn line_ranges(&self, range: TextRange) -> Range<usize> {
         let start = self.line_for_offset(range.start()).unwrap();
         let end = self.line_for_offset(range.end().checked_sub(1.into()).unwrap()).unwrap();
-        (start as usize)..(end as usize)
+        (start as usize)..(end as usize) + 1
     }
 }
 
