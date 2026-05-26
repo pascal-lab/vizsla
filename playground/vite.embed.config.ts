32e43b3e019c8cba7892659fda7cede5e9f6e041
hongjr03 <hongjr03@gmail.com>
2026-05-27 01:21:14 +0800

    fix(docs): prepare playground embed assets

--- playground/vite.embed.config.ts
+++ playground/vite.embed.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
       name: "VideLab",
       formats: ["es"],
       fileName: (format) => `vide-lab.${format}.js`,
+      cssFileName: "vide-playground",
     },
   },
   worker: {
